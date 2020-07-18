e938607a36ad4fc88a8e87e61f7c2dec41882b2c
zushicat <karinhoehne@akasushi.de>
2020-07-18 12:28:36 +0200

    uncomment function calls

--- src/create_data.py
+++ src/create_data.py
@@ -45,42 +45,42 @@ def _load_list_tmp_data(file_name: str) -> List[Dict[str, Any]]:
 # *******
 # 1 - compute some geo data
 # *******
-# create_suburb_polygons()
-# find_neighbouring_suburbs()  # needed for more efficient tree neighbour processing; keep uncommented until 4. is done
+create_suburb_polygons()
+find_neighbouring_suburbs()  # needed for more efficient tree neighbour processing; keep uncommented until 4. is done
 
 # *******
 # 2 - create base datasets from original "Baumkataster" csv data
 # *******
-# _save_tmp_data("data_2017.jsonln", process_dataset_2017())
-# _save_tmp_data("data_2020.jsonln", process_dataset_2020())
+_save_tmp_data("data_2017.jsonln", process_dataset_2017())
+_save_tmp_data("data_2020.jsonln", process_dataset_2020())
 
 # *******
 # 3 - merge datasets 2017 / 2020
 # *******
-# datasets = _load_tmp_data()
-# _save_tmp_data("data_merged.jsonln", merge_datasets(datasets))
+datasets = _load_tmp_data()
+_save_tmp_data("data_merged.jsonln", merge_datasets(datasets))
 
-# merged_data = _load_list_tmp_data("data_merged.jsonln")
+merged_data = _load_list_tmp_data("data_merged.jsonln")
 
 # *******
 # 4 - process neighbour trees in radius, then clean up pairs of close trees (< 2m)
 # *******
-# close_pairs, all_pairs = process_tree_neighbours(merged_data)  # takes approx. 20 minutes
-# _save_tmp_data("neighbours_close_pairs.jsonln", close_pairs)
-# _save_tmp_data("neighbours_all_pairs.jsonln", all_pairs)
+close_pairs, all_pairs = process_tree_neighbours(merged_data)  # takes approx. 20 minutes
+_save_tmp_data("neighbours_close_pairs.jsonln", close_pairs)
+_save_tmp_data("neighbours_all_pairs.jsonln", all_pairs)
 
-# close_pair_list = _load_list_tmp_data("neighbours_close_pairs.jsonln")  # takes approx. 10-20 secs.
-# merged_data = cleanup_close_pairs(merged_data, close_pair_list)
-# _save_tmp_data("data_merged_cleanup.jsonln", merged_data)
+close_pair_list = _load_list_tmp_data("neighbours_close_pairs.jsonln")  # takes approx. 10-20 secs.
+merged_data = cleanup_close_pairs(merged_data, close_pair_list)
+_save_tmp_data("data_merged_cleanup.jsonln", merged_data)
 
 # *******
 # 5 - predict genus and/or age resp. age_group by clusters of neighbouring trees
 # *******
-# merged_data = _load_list_tmp_data("data_merged_cleanup.jsonln")
-# neighbours_pairs = _load_list_tmp_data("neighbours_all_pairs.jsonln")
+merged_data = _load_list_tmp_data("data_merged_cleanup.jsonln")
+neighbours_pairs = _load_list_tmp_data("neighbours_all_pairs.jsonln")
 
-# merged_data_with_predictions = predict_genus_age(merged_data, neighbours_pairs)
-# _save_tmp_data("data_merged_with_predictions.jsonln", merged_data_with_predictions)
+merged_data_with_predictions = predict_genus_age(merged_data, neighbours_pairs)
+_save_tmp_data("data_merged_with_predictions.jsonln", merged_data_with_predictions)
 
 # *******
 # 6 - write compressed exports to /data/exports
